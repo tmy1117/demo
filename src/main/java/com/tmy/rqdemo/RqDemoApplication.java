5ce56e829859c63e61950bff0e0c105bd86f1db3
muyang.tian <muyang.tian@joowing.com>
2021-03-19 14:45:01 +0800

    提交

--- src/main/java/com/tmy/rqdemo/RqDemoApplication.java
+++ src/main/java/com/tmy/rqdemo/RqDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RqdemoApplication {
+public class RqDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RqdemoApplication.class, args);
+        SpringApplication.run(RqDemoApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/tmy/rqdemo/reposstory/UserMapper.java
@@ -0,0 +1,12 @@
+package com.tmy.rqdemo.reposstory;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author: muyang.tian
+ * @Date: 2021-03-19 14:40
+ */
+@Repository
+public class UserMapper{
+
+}
