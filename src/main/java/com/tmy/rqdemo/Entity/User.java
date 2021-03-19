9aa7573aeb42af9e4de9522f54d7a781ad610147
muyang.tian <muyang.tian@joowing.com>
2021-03-19 14:36:36 +0800

    first

--- src/main/java/com/tmy/rqdemo/Entity/User.java
+++ src/main/java/com/tmy/rqdemo/Entity/User.java
@@ -1,9 +1,28 @@
 package com.tmy.rqdemo.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author: muyang.tian
  * @Date: 2021-03-19 14:07
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    /*
+     * 姓名
+     */
+    private String name;
+    /*
+     * 年龄
+     */
+    private Integer age;
+    /*
+     * 手机号
+     */
+    private Integer phone;
 
 }
