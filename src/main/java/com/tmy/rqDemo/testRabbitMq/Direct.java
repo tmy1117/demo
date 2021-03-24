731647180cdbfa5c55c82b5c85628a0350307a40
muyang.tian <tmy1117@163.com>
2021-03-24 19:47:58 +0800

    嚯哗

--- src/main/java/com/tmy/rqDemo/testRabbitMq/Direct.java
+++ src/main/java/com/tmy/rqDemo/testRabbitMq/Direct.java
@@ -5,25 +5,25 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 
 import java.io.IOException;
+
 import java.util.concurrent.TimeoutException;
 
 /**
  * @author: muyang.tian
- * @Date: 2021-03-24 17:07
- * 广播类型的消息
+ * @Date: 2021-03-24 17:44
  */
-public class Fanout {
+public class Direct {
 
-    private final static String EXCHANGE_NAME = "test_fanout";
+    private final static String EXCHANGE_NAME = "test_direct";
 
     public static void main(String[] args) {
         try {
             Connection connection = ConnectionUtil.GetConnection();
             Channel channel = connection.createChannel();
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
-            String massage = "注册成功!";
-            channel.basicPublish(EXCHANGE_NAME,"",null,massage.getBytes());
-            System.out.println("[生产者] :"+massage);
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            String massage = "注册成功！请短信回复[T]退订";
+            channel.basicPublish(EXCHANGE_NAME,"email",null,massage.getBytes());
+            System.out.println("[sent] :"+massage);
             channel.close();
             connection.close();
         } catch (IOException | TimeoutException e) {
---
+++ src/main/java/com/tmy/rqDemo/testRabbitMq/SecondDirectRecv.java
@@ -0,0 +1,35 @@
+package com.tmy.rqDemo.testRabbitMq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author: muyang.tian
+ * @Date: 2021-03-24 18:03
+ */
+public class SecondDirectRecv {
+
+    private final static String EXCHANGE_NAME = "test_direct";
+    private final static String QUEUE_NAME = "direct_exchange_queue_email";
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = ConnectionUtil.GetConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+            channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"email");
+            DefaultConsumer consumer = new DefaultConsumer(channel){
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body){
+                    String msg = new String(body);
+                    System.out.println("[邮箱服务] :"+msg);
+                }
+            };
+            channel.basicConsume(QUEUE_NAME,true,consumer);
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
